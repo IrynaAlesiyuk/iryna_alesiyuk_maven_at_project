115993e596650b82e1884c5bea5f472178424a40
Iryna Alesiyuk <oira2008@yandex.ru>
2023-11-26 11:02:05 +0300

    unit tests

---
+++ src/test/java/unitTests/DayOfWeekUtilityGetDayOfWeekNextYearJUnitTest.java
@@ -0,0 +1,12 @@
+package unitTests;
+
+import org.junit.Test;
+
+public class DayOfWeekUtilityGetDayOfWeekNextYearJUnitTest {
+
+    @Test
+    public void test(){
+        String actual = DayOfWeekUtility.getDayOfWeekNextYear(4);
+        System.out.println(actual);
+    }
+}
