31a8a5a03bfaeb4a027026dd1f32959b22b84064
Iryna Alesiyuk <oira2008@yandex.ru>
2023-11-27 20:42:53 +0300

    class work and hw-1

--- src/test/java/seleniumTests/SeleniumTest.java
+++ src/test/java/seleniumTests/SeleniumTest.java
@@ -1,22 +1,45 @@
 package seleniumTests;
 
+import lombok.extern.log4j.Log4j2;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.net.MalformedURLException;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.List;
+import java.util.Locale;
 
 public class SeleniumTest {
     public static void main(String[] args) throws MalformedURLException, InterruptedException {
-      //   WebDriver driver = new RemoteWebDriver(new URL("http://localhost:9515"), new ChromeOptions());
+        //   WebDriver driver = new RemoteWebDriver(new URL("http://localhost:9515"), new ChromeOptions());
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        Thread.sleep(500);
         driver.get("https://google.com");
-      //  driver.findElement(By.name("q")).sendKeys("погода минск");
-      //  Thread.sleep(500);
-      //  driver.findElement(By.xpath("//ul[@role='listbox'/li(1)"));
+        driver.findElement(By.xpath("(//div[contains(text(), 'Принять все')])[2]")).click();
 
-       // List<WebElement> list= driver.findElements(By.xpath("//div[@aria-label='суббота']/../.."));
-      //  list.get(1).click();
-       // driver.findElement(By.xpath("//div[contains"));
+        driver.findElement(By.name("q")).sendKeys("погода минск");
+        Thread.sleep(500);
+        driver.findElement(By.xpath("//ul[@role='listbox']/li[1]")).click();
+        Thread.sleep(1000);
+
+        List<WebElement> list = driver.findElements(By.cssSelector("[data-wob-di]"));
+        Thread.sleep(1000);
+        list.get(1).click();
+
+        Locale locale = Locale.getDefault();
+        LocalDate currentDate = LocalDate.now().plusDays(2);
+        String dayOfWeek = currentDate.getDayOfWeek().getDisplayName(TextStyle.FULL, locale);
+
+        WebElement element = driver.findElement(By.xpath(String.format("//*[contains(@aria-label,'Celsius %s 12:00')][1]", dayOfWeek.toLowerCase())));
+
+        String[] parts = element.getAttribute("aria-label").split("°");
+        System.out.println("Погода на завтра в 12: " + parts[0]);
+
+        driver.quit();
 
     }
 }
---
+++ src/test/java/seleniumTests/WildberriesTest1.java
@@ -0,0 +1,41 @@
+package seleniumTests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WildberriesTest1 {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Устанавливаем время ожидания в 10 секунд
+
+        driver.manage().window().maximize();
+        driver.get("https://www.wildberries.by");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(text(), 'Принять')]"))).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[aria-label='Главное меню']"))).click();
+        Thread.sleep(500);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(), 'Дом')]"))).click();
+        driver.findElement(By.xpath("//span[contains(text(), 'Гостиная')]")).click();
+        Thread.sleep(1000);
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(), 'Коробки для хранения')]"))).click();
+        Thread.sleep(1000);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//button[@aria-label='Фильтры'])[1]"))).click();
+        //  WebElement filters = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//button[@aria-label='Фильтры'])[1]/span")));
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(), 'до 5 дней')]"))).click();
+
+        // String foundGoods = driver.findElement(By.cssSelector(".filters-sidebar__total-desktop")).getText();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".filters-sidebar__close"))).click();
+        Thread.sleep(1000);
+        String foundGoods = driver.findElement(By.cssSelector(".total-goods")).getText();
+
+        System.out.println(foundGoods);
+    }
+}
